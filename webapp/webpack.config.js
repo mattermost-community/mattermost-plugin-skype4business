8c55a965e570b71423417705c9eb9ad2f5914f23
Christopher Speller <crspeller@gmail.com>
2019-07-23 09:34:21 -0700

    MM-17036 Updating dependencies and build system. (#15)

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -19,10 +19,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react'],
                         plugins: [
-                            'transform-class-properties',
-                            'transform-object-rest-spread',
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-syntax-dynamic-import',
+                        ],
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    chrome: 66,
+                                    firefox: 60,
+                                    edge: 42,
+                                    safari: 12,
+                                },
+                                modules: false,
+                                debug: false,
+                                useBuiltIns: 'usage',
+                                shippedProposals: true,
+                            }],
+                            ['@babel/preset-react', {
+                                useBuiltIns: true,
+                            }],
                         ],
                     },
                 },
