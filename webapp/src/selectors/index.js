4c0672950b52856c23ae506ae7a138a480757f9a
sanjay12345 <sanjaymj824@gmail.com>
2021-06-11 17:20:11 +0530

    add url

--- webapp/src/selectors/index.js
+++ webapp/src/selectors/index.js
@@ -1,10 +1,29 @@
 import {id as pluginId} from '../manifest';
 
+import { getConfig } from 'mattermost-redux/selectors/entities/general';
+
 const getPluginState = (state) => state['plugins-' + pluginId] || {};
 
 const getAuthenticationResult = (state) => getPluginState(state).authenticationResult;
 
+export const getPluginServerRoute = (state) => {
+    const config = getConfig(state);
+
+    let basePath = '';
+    if (config && config.SiteURL) {
+        basePath = new URL(config.SiteURL).pathname;
+
+        if (basePath && basePath[basePath.length - 1] === '/') {
+            basePath = basePath.substr(0, basePath.length - 1);
+        }
+    }
+
+    // return basePath + '/plugins/' + PluginId;
+    return basePath;
+};
+
 export default {
     getAuthenticationResult,
 };
+
 
