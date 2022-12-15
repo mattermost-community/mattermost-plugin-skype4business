d106c331aae4c2abf88f4dd5ec0998a02547282f
Ben Schumacher <ben.schumacher@mattermost.com>
2022-12-15 13:50:46 +0100

    Update HTTP client (#90)

--- server/client.go
+++ server/client.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/tls"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -15,18 +14,14 @@ import (
 
 // Client is a new HTTP Client to talk to the Skype server
 type Client struct {
-	httpClient *http.Client
-	log        ILogger
+	*http.Client
+	log ILogger
 }
 
 // NewClient returns a new Client
 func NewClient() *Client {
 	return &Client{
-		httpClient: &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		},
+		Client: http.DefaultClient,
 	}
 }
 
@@ -37,7 +32,7 @@ func (c *Client) setLogger(logger ILogger) {
 func (c *Client) authenticate(url string, body url.Values) (*AuthResponse, error) {
 	c.log.LogDebug("Request in authenticate", "url", url, "body", body)
 
-	resp, err := c.httpClient.PostForm(url, body)
+	resp, err := c.PostForm(url, body)
 	if err != nil {
 		return nil, err
 	}
@@ -137,7 +132,7 @@ func (c *Client) performRequestAndGetAuthHeader(url string) (*string, error) {
 
 	c.logRequest("performRequestAndGetAuthHeader", req, false)
 
-	resp, err := c.httpClient.Do(req)
+	resp, err := c.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -154,7 +149,7 @@ func (c *Client) performRequestAndGetAuthHeader(url string) (*string, error) {
 }
 
 func (c *Client) do(req *http.Request, v interface{}) error {
-	resp, err := c.httpClient.Do(req)
+	resp, err := c.Do(req)
 	if err != nil {
 		return err
 	}
